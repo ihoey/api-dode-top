162b88c8a1bb249f396cc7aec94140d7f3b02d61
ihoey <mail@ihoey.com>
2018-12-14 15:10:51 +0800

    feat: pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+  apps: [{
+    name: 'iotp',
+    script: 'index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '100M',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy: {
+    production: {
+      user: 'root',
+      host: '118.24.72.231',
+      ref: 'origin/master',
+      repo: 'git@github.com:ihoey/api-dode-top.git',
+      path: '/data/www/iotp',
+      'post-deploy': 'yarn && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
